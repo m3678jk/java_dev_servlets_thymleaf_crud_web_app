27ab1565b389d0618d952d1b36b7cb6d36699503
JuliaM88 <julia.moskalenko@outlook.com>
2022-07-07 10:34:30 +0200

    add comments

--- src/main/java/servletsThymeleaf/Setting.java
+++ src/main/java/servletsThymeleaf/Setting.java
@@ -1,5 +1,9 @@
 package servletsThymeleaf;
 
 public class Setting {
+
+    // don't use backslash in your path
+    // because it's only compatible with Windows system
+    // but the forwarslash (/) works with Unix and Windows
     public static final String PATH_TO_TEMPLATES = ("src\\main\\resources\\templates\\");
 }
